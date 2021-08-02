7ca2da123a491c73403de284508b392be36b0a8b
Aman <amansharma.as974@gmail.com>
2021-08-03 00:20:25 +0530

    Fuck

--- src/Section-2/LogInAdmin.js
+++ src/Section-2/LogInAdmin.js
@@ -7,6 +7,14 @@ import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 import Button from '@material-ui/core/Button'
 import '../App.css'
 function LogInAdmin() {
+    function dropDownMenu(){
+        let dropdown = document.querySelector('.admin-drop-down-menu');
+        if(dropdown.style.display === 'block'){
+            dropdown.style.display = 'none'
+        }else {dropdown.style.display = 'block'}
+        
+    }
+
     return (
         <div>
             <div class="admin-section">
@@ -19,8 +27,10 @@ function LogInAdmin() {
                                     <div className="admin-image"></div>
                                     <div>
                                         <h1 className="admin-name">Vicky Kaushal</h1>
-                                        <p>Admin <ArrowDropDownIcon className="admin-drop-down" />   </p>
-                                        
+                                        <p>Admin <ArrowDropDownIcon onClick={dropDownMenu} className="admin-drop-down" />   </p>
+                                        <div className="admin-drop-down-menu">
+
+                                        </div>
                                     </div>
                                 </div>
                         </div>
