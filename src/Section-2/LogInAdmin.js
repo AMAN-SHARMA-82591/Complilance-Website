f1625428d886d3d8ce2f0ccfef9a2f2367c43f3c
Aman <amansharma.as974@gmail.com>
2021-08-03 15:46:19 +0530

    F

--- src/Section-2/LogInAdmin.js
+++ src/Section-2/LogInAdmin.js
@@ -3,15 +3,23 @@ import EmailIcon from '@material-ui/icons/Email';
 import SettingsIcon from '@material-ui/icons/Settings';
 import AlarmIcon from '@material-ui/icons/Alarm';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 import Button from '@material-ui/core/Button'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import '../App.css'
 function LogInAdmin() {
+    
     function dropDownMenu(){
+        let dropLogo = document.querySelector('.admin-drop-down')
         let dropdown = document.querySelector('.admin-drop-down-menu');
         if(dropdown.style.display === 'block'){
             dropdown.style.display = 'none'
-        }else {dropdown.style.display = 'block'}
+            dropLogo.style.transform = 'rotate(0deg)';
+        }else {
+            dropdown.style.display = 'block'
+            dropLogo.style.transform = 'rotate(180deg)';
+
+    }
         
     }
 
@@ -27,9 +35,30 @@ function LogInAdmin() {
                                     <div className="admin-image"></div>
                                     <div>
                                         <h1 className="admin-name">Vicky Kaushal</h1>
-                                        <p>Admin <ArrowDropDownIcon onClick={dropDownMenu} className="admin-drop-down" />   </p>
+                                        <p>Admin <ArrowDropDownIcon  onClick={dropDownMenu} className="admin-drop-down" />   </p>
                                         <div className="admin-drop-down-menu">
-
+                                                <div className="drop-down-menu-admin-items">
+                                                        <div className="drop-down-sec-logo-cont1"></div>
+                                                        <div className="drop-down-sec-logo-cont2">
+                                                            <h1>Vicky Kaushal</h1>
+                                                            <p>Admin</p>
+                                                        </div>
+                                                        <div className="drop-down-sec-help-cont3">
+                                                            <p>Help </p>
+                                                           <HelpOutlineIcon fontSize="small" />
+                                                        </div>
+                                                </div>
+                                                <div className="drop-down-menu-admin-text-items">
+                                                    <p>Delhi Land & Finance Ltd.</p>
+                                                </div>
+                                                <div className="drop-down-menu-admin-button-items">
+                                                    <button>Manage Account</button>
+                                                </div>
+                                                <div className="drop-down-menu-admin-add-comp-items">
+                                                    <AddCircleOutlineIcon />
+                                                    <p> Add Another 
+                                                    Company</p>
+                                                </div>
                                         </div>
                                     </div>
                                 </div>
